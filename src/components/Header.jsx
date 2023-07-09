e0268b8a5cac83a91b9279a7e3467048f4fb7038
Reid M <reidmadock@gmail.com>
2023-07-08 20:41:30 -0500

    slight style changes, nav tabs centered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Navigation from './Navigation';
 import Home from './Home';
+import { Typography } from '@mui/material';
 
 export default function Header() {
   return (
     <>
       <div className='Header'>
-        <h1>Reid Madock</h1>
+        <Typography component={'h2'} fontStyle={'italic'} fontSize={'3rem'} fontWeight={600}>
+          Reid Madock
+        </Typography>
         <Navigation/>
       </div>
     </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -29,9 +29,9 @@ function CustomTabPanel(props) {
 }
 
 CustomTabPanel.propTypes = {
-children: PropTypes.node,
-index: PropTypes.number.isRequired,
-value: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
@@ -50,26 +50,26 @@ export default function Navigation() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 3, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="Navigation" textColor='#e76b74'>
-          <Tab label="Home" {...a11yProps(0)} />
-          <Tab label="Portfolio" {...a11yProps(1)} />
-          <Tab label="Resume" {...a11yProps(2)} />
-          <Tab label="Contact" {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        <Home />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <Portfolio />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <Resume />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={3}>
-        <Contact />
-      </CustomTabPanel>
+        <Box sx={{ borderBottom: 3, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="Navigation" textColor='#e76b74' centered>
+                <Tab label="Home" {...a11yProps(0)} />
+                <Tab label="Portfolio" {...a11yProps(1)} />
+                <Tab label="Resume" {...a11yProps(2)} />
+                <Tab label="Contact" {...a11yProps(3)} />
+            </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+            <Home />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+            <Portfolio />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+            <Resume />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={3}>
+            <Contact />
+        </CustomTabPanel>
     </Box>
   );
 }
